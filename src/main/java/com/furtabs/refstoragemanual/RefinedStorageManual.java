55633a4f02001cb240e63bb31803479905c8297f
Tabitha Hanegan <tabithahanegan@gmail.com>
2025-05-03 15:33:33 -0400

    rewrite contents to better match RS1 wiki

---
+++ src/main/java/com/furtabs/refstoragemanual/RefinedStorageManual.java
@@ -0,0 +1,6 @@
+package com.furtabs.refstoragemanual;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("refstoragemanual")
+public class RefinedStorageManual {}
--- src/main/java/com/naylahanegan/refstoragemanual/RefinedStorageManual.java
+++
@@ -1,6 +0,0 @@
-package com.naylahanegan.refstoragemanual;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod("refstoragemanual")
-public class RefinedStorageManual {}
